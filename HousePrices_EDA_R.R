4a9f74f582927eb593ab767e553a4c3ad60c38c5
Nicolas SEVERINO <nicolasseverino@outlook.com>
2019-12-09 11:22:31 +0100

    Initial commit

---
+++ HousePrices_EDA_R.R
@@ -0,0 +1,192 @@
+# librairies
+library(dplyr)
+library(ggplot2)
+library(FactoMineR)
+library(factoextra)
+library(corrplot)
+
+# Chargement
+train <- read.csv("C:/Users/utilisateur/Downloads/House Prices/train.csv")
+
+# Dimensions
+dim(train)
+
+# Summary
+summary(train)
+
+# Ah... Il y a beaucoup de données manquantes.
+# Combien par colonne?
+# nb de donnees manquantes par colonne
+colSums(is.na(train))
+# Je me rends compte que les colonnes avec beaucoup de NA
+# correspondent à la présence/l'absence d'un attribut particulier
+# dans la maison. Il serait intéressant de mettre ces colonnes
+# A part pour en faire une analyse hors-ACP, parce que
+# dans l'ACP, toute ligen avec au moins un NA est éliminée.
+# Cependant, l'analyse de ces présence/absence d'attribut
+# peut être très intéressante d'un point de vue explication/prédiction
+# de SalesPrice
+
+# Etude de SalePrice
+hist(train$SalePrice)
+# Il y a une queue de distribution sur la droite
+# == il y a certaines maisons beaucoup trop chères
+# comparées aux autres
+# Statistiquement il est plus intéressant de travailler
+# sur des données normales (ou gaussiennes)
+# Transformation des données
+train$logSalePrice<-log(train$SalePrice)
+hist(log(train$SalePrice))
+
+# Etude des attributs rares et leur impact sur SalesPrice
+# Récupération des colonnes avec NA > 300
+AttributRare<-train[,c(which(colSums(is.na(train))>300),82)]
+head(AttributRare)
+summary(AttributRare)
+# La valeur NA dans ces colonnes porte une information
+# importante : absence de la feature!
+# REmplaçons par "none"
+levels(AttributRare$Alley)<-c(levels(AttributRare$Alley),"none")
+levels(AttributRare$FireplaceQu)<-c(levels(AttributRare$FireplaceQu),"none")
+levels(AttributRare$PoolQC)<-c(levels(AttributRare$PoolQC),"none")
+levels(AttributRare$Fence)<-c(levels(AttributRare$Fence),"none")
+levels(AttributRare$MiscFeature)<-c(levels(AttributRare$MiscFeature),"none")
+AttributRare[is.na(AttributRare)] <- "none"
+# Box plots
+qplot(Alley, logSalePrice, data=AttributRare, 
+      geom="boxplot")
+qplot(FireplaceQu, logSalePrice, data=AttributRare, 
+      geom="boxplot")
+qplot(PoolQC, logSalePrice, data=AttributRare, 
+      geom="boxplot")
+qplot(Fence, logSalePrice, data=AttributRare, 
+      geom="boxplot")
+qplot(MiscFeature, logSalePrice, data=AttributRare, 
+      geom="boxplot")
+
+
+# On passe à l'autre partie du jeu de données
+# Elimination des colonnes avec NA > 300
+TrainFull<-train[,which(colSums(is.na(train))<300)]
+dim(TrainFull)
+TrainFull<-TrainFull[complete.cases(TrainFull),]
+
+# Nombre de lignes restantes entières
+# après élimination
+# des colonnes avec données manquantes
+length(which(complete.cases(TrainFull)==T))
+
+# Récupération des colonnes numériques
+trainNum<-select_if(TrainFull, is.numeric)
+dim(trainNum)
+
+# Summary
+summary(trainNum)
+# JE me rends compte qu'il y a pas mal de
+# 3rd Quartile à zéro. Cela dénote 
+# des variables avec beaucoup de zéro.
+
+# Jeter un coup d'oeil aux variables avec 
+# Beaucoup de zéros
+colSums(trainNum == 0, na.rm=T)
+
+
+# Var. déjà exploitées : PoolArea, MiscVal, FirePlaces.
+# On les élimine.
+
+# Var. qui dépendent de la présence/absence d'une
+# feature : ScreenPorch, X3SsnPorch, EnclosedPorch,
+#           OpenPorchSF, WoodDeckSF, HalfBath, BsmtHalfBath
+#           LowQualFinSF, BsmtFullBath, BsmtFinSF2, BsmtFinSF1
+# Solution 1: transformer en présence/absence de feature
+#             et dessiner des boxplots de logSalesPrce
+#             en fonction de chaque feature binarisée,
+#             puis éliminer les colonnes pour l'ACP'
+# Solution 2: créer des classes de score pour chaque feature
+#             exemple : 0 pour 0, 1 pour 0 à 150, 2 pour 150 à
+#             + l'infini. Définir les intervalles en se basant
+#             sur les médianes/quartiles. Faire des boxplots aussi.
+
+# Var. quali : MoSold. Solution : éliminer de l'ACP, 
+#              Etudier à part logSalePrice en fonction de MoSold
+#              Ou de MoSold réparti en saisons plus grossières
+trainNum$MoSoldFac<-factor(trainNum$MoSold)
+
+qplot(MoSoldFac, logSalePrice, data=trainNum, 
+      geom=c("violin","boxplot"))
+qplot(MoSold, logSalePrice, data=trainNum,
+      geom=c("point", "smooth"))
+
+
+# Eliminer toutes les colonnes avec >>0 pour l'ACP
+# Elimination également des MoSold et MoSoldFac
+colSums(trainNum == 0, na.rm=T)
+dataACP<-trainNum[,c(which(colSums(trainNum == 0, na.rm=T)<300))]
+dataACP<-select(dataACP, -MoSold,-MoSoldFac, -Id, -SalePrice)
+dim(dataACP)
+
+# ACP
+cor(dataACP)
+res.pca<-PCA(dataACP)
+fviz_pca_var(res.pca, select.var=list(contrib=10),repel=T)
+# Un faisceau de variables lié aux garages est corrélé
+# à logSalePrice
+# La surface à l'étage ne semble pas jouer sur le prix
+# Colorons par quartier, sur un biplot
+fviz_pca_biplot(res.pca,select.var=list(contrib=10),
+                select.ind=list(contrib=200),
+                label="var",
+                col.ind=TrainFull$Neighborhood)
+
+# Dimensions 3 et 4:
+fviz_pca_biplot(res.pca, axes=c(3,4), select.var=list(contrib=10),
+                select.ind=list(contrib=200),
+                label="var",
+                col.ind=TrainFull$Neighborhood)
+
+library(coefplot)
+library(visreg)
+library(car)
+reg.multiple <- lm(logSalePrice~., data = dataACP)
+vif(reg.multiple)
+visreg(reg.multiple)
+plot(reg.multiple,1)
+coefplot(reg.multiple)
+step(reg.multiple, direction="backward")
+summary(step(reg.multiple, direction="backward"))
+
+
+#on peut enlever FullBath de nos features car moins pertinente
+dataACP<-select(dataACP, -FullBath)
+res.pca<-PCA(dataACP)
+#Variance en pourcentage pour chaque dimension
+eigenvalue <- get_eigenvalue(res.pca)
+fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 50))
+
+
+var<-get_pca_var(res.pca)
+head(var$cos2, 4)
+corrplot(var$cos2, is.corr=FALSE)
+fviz_cos2(res.pca, choice = "var", axes = 1:2)
+fviz_pca_var(res.pca, col.var = "cos2",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE # �vite le chevauchement de texte
+)
+head(var$contrib, 4)
+corrplot(var$contrib, is.corr=FALSE)
+# Contributions des variables � PC1
+fviz_contrib(res.pca, choice = "var", axes = 1, top = 10)
+# Contributions des variables � PC2
+fviz_contrib(res.pca, choice = "var", axes = 2, top = 10)
+fviz_contrib(res.pca, choice = "var", axes = 1:4, top = 10)
+
+fviz_pca_var(res.pca, col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel=TRUE
+)
+lm(fviz_contrib(res.pca, choice = "var", axes = 1:4, top = 10))
+boxOut <- boxplot(dataACP)
+boxOut$out
+b2<-dataACP[-which(dataACP%in%boxOut$out)]
+dataACP[which(dataACP %in% boxplot.stats(dataACP)$out)]
+
---
+++ README.md
@@ -0,0 +1,32 @@
+# HOUSE PRICES
+
+Dans le cadre de la formation Data / IA à l'Ecole IA Microsoft (Powered by Simplon), il a été demandé de travailler sur un projet d'analyse prédictive.
+D'abord effectué directement sur la plateforme en ligne [BigML](https://bigml.com/), l'ensemble de processus d'apprentissage automatique supervisé a du être réalisé sur Python grâce à Jupyter Notebook.
+
+**But :** pouvoir établir une prédiction et transmettre le résultat sur [Kaggle](https://www.kaggle.com/), une plateforme web organisant des compétitions en science des données.
+
+Pour retrouver la compétition en question, vous retrouverez tous les renseignements et datasets ainsi que le classement directement sur cette page : [HousePrices](https://www.kaggle.com/c/house-prices-advanced-regression-techniques/overview).
+
+## Pré-requis
+
+Le script en question ne sera fonctionnel qu'après un certain nombre d'outils :
+* [Anaconda](https://www.anaconda.com/distribution/) ;
+* Un compte sur Kaggle ;
+* Un compte sur GitHub si vous souhaitez push votre projet en ligne et de façon publique ;
+* Importation de modules :
+  * Numpy : manipulation de matrices et de tableaux multidimensionnels ;
+  * Kaggle : API pour transmettre le résultat de notre algorithme et participer à la compétition ;
+  * Pandas : bibliotèque Python permettant la manipulation et l'analyse des données ;
+  * Keras / Tensorflow : deep-learning ;
+  * Matplotlib : pour créer une visualisation graphique des données.
+
+## Composition du projet
+
+* Exploratory Data Analysis (EDA) en R ;
+* Une EDA avec Python ;
+* Une prédiction avec SkLearn ;
+* Une prédiction avec Keras / Tensorflow.
+
+## Auteur
+
+* **Nicolas SEVERINO** - [Autres projets](https://github.com/nicolasseverino/)
